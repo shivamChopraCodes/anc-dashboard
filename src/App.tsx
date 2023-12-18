cd14ef37999eaa9864725e1b038d72adeccfa56a
Shivam Chopra <shivamchopra@gokwik.co>
2023-12-19 00:37:55 +0530

    chores: Added socials

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,18 @@ import Dashboard from "./pages/home"
 function App() {
   return (
     <div className="App">
+      <div className="w-full p-4 flex justify-between ">
+        <a href="mailto:contactshivamchopra@gmail.com">
+          Email:{" "}
+          <span className="text-blue-600 underline">
+            contactshivamchopra@gmail.com
+          </span>
+        </a>
+        <a href="https://www.linkedin.com/in/shivam-chopra-22688b147/">
+          Linkedin:{" "}
+          <span className="text-blue-600 underline">Shivam Chopra</span>
+        </a>
+      </div>
       <Dashboard />
     </div>
   )
